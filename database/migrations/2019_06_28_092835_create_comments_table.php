e14dc3f9703a4e602c62f2af46a16e89ae186b85
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2019-06-28 14:16:26 +0430

    comment migration added

--- database/migrations/2019_06_28_092835_create_comments_table.php
+++ database/migrations/2019_06_28_092835_create_comments_table.php
@@ -15,8 +15,9 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
-
             $table->timestamps();
+            $table->longText('message');
+            $table->boolean('approved');
         });
     }
 
